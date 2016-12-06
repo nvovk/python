1c95a5e5423b0be3b8272341fd210c2eb749917e
Natalia Vovk <n.vovk.forte@gmail.com>
2016-12-06 19:51:07 +0200

    edit 4

--- Project Euler/4/index.py
+++ Project Euler/4/index.py
@@ -1,8 +1,17 @@
 from checkpol import CheckPol
 
-lst = list(str(999*999))
-pol = CheckPol(lst)
-print(pol)
+num = 999 * 999
+
+while True:
+    pol = CheckPol(list(str(num)))
+    if pol == None:
+        num = num - 1
+        False
+    else:
+        True
+
+print(num)
+
 
 
 
