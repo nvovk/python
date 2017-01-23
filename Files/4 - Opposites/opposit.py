f1141a74e53791d4ad729037745cfeb2c80929f7
Natalia Vovk <n.vovk.forte@gmail.com>
2017-01-23 10:22:11 +0200

    file 4

--- Files/4 - Opposites/opposit.py
+++ Files/4 - Opposites/opposit.py
@@ -1,8 +1,8 @@
 def pairs(l):
     p = 0
-    for i in range(0, len(l)):
-        for j in range(0, len(l)):
-            if int(l[i]) + int(l[j]) == 0 and abs(int(l[i])) == abs(int(l[j])):
+    for i in l:
+        for j in l:
+            if int(i) + int(j) == 0 and abs(int(i)) == abs(int(j)):
                 p += 1
     if p > 0:
         return True
@@ -12,11 +12,19 @@ def pairs(l):
 
 def opp(l):
     if pairs(l) is True:
-        for i in range(0, len(l) - 1):
-            for j in range(0, len(l) - 1):
-                if int(l[i]) + int(l[j]) == 0 and abs(int(l[i])) == abs(int(l[j])):
-                    l.remove(l[i])
-                    l.remove(l[j])
+        for i in l:
+            for j in l:
+                if int(i) + int(j) == 0 and abs(int(i)) == abs(int(j)):
+                    print(i,' , ',j)
+                    l.remove(i)
+                    l.remove(j)
                     opp(l)
     else:
+        print('tst ', l)
         return l
+
+l = [1, 2, 2, 3, 4, 4]
+l1 = [1, 2, 3, 3, -2, -1, 5]
+
+print(opp(l))
+print(opp(l1))
